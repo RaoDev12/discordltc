0de645d6d696f813eb69b7585c962b17631d4f6d
RaoDev12 <129713863+RaoDev12@users.noreply.github.com>
2024-04-25 18:53:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RaoDev12/discordltc",
-    packages=['discordltc'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
