15a30cb389d915df2e3223954ae4d13e0f8d7207
RaoDev12 <129713863+RaoDev12@users.noreply.github.com>
2024-04-25 18:19:10 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discordltc",
+    version="1.0.0",
+    author="maker",
+    description="discordltc is a very useful library made to be used with discord.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">= 3.6",
+    include_package_data=True,
+    install_requires=["discord.py"]
+)
